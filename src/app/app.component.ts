23334e3fd5f832f1fa40c829c9610f1263c0aaec
jmcaballerog <jmcaballerog@minsait.com>
2022-05-11 16:08:50 +0200

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-library';
+
+  displayedColumns: string[] = ['mediaName', 'mediaType', 'mediaDate', 'mediaEdit', 'mediaDelete'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog : MatDialog, private api : ApiService){
+
+  }
+  ngOnInit(): void {
+    this.getAllMedia();
+    this.changePaginationLanguage();
+  }
+  
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width:'30%'
+    }).afterClosed().subscribe(val=>{
+      if(val === 'save'){
+        this.getAllMedia();
+      }
+    });
+  }
+
+  getAllMedia(){
+    this.api.getMedia().subscribe({
+      next:(res)=>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+        error:(err)=>{
+          alert("Error al recuperar multimedia")
+        }
+    })
+  }
+
+  changePaginationLanguage(){
+    this.paginator._intl.itemsPerPageLabel = "Elementos por página";
+    this.paginator._intl.nextPageLabel = "Siguiente página";
+    this.paginator._intl.previousPageLabel = "Página anterior";
+    this.paginator._intl.lastPageLabel = "Última página";
+    this.paginator._intl.firstPageLabel = "Primera página";
+    this.paginator._intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+      if (length === 0 || pageSize === 0) {
+        return '0 de ' + length;
+      }
+
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      // If the start index exceeds the list length, do not try and fix the end index to the end.
+      const endIndex = startIndex < length ?
+        Math.min(
+          startIndex + pageSize, length) :
+        startIndex + pageSize;
+      return startIndex + 1 + ' - ' + endIndex + ' de ' + length;
+    };
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  editMedia(row : any){
+    this.dialog.open(DialogComponent,{
+      width:'30%',
+      data:row
+    }).afterClosed().subscribe(val=>{
+      if(val === 'update'){
+        this.getAllMedia();
+      }
+    })
+  }
+
+  deleteMedia(id : number){
+    this.api.deleteMedia(id).subscribe({
+      next:(res)=>{
+        alert("Borrado con éxito");
+        this.getAllMedia();
+      },
+      error:()=>{
+        alert("Error al borrar");
+      }
+    })
+    
+  }
+
+
 }
